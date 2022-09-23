4c79ce2366d3569ee0075e9fd5bd11fbc9b229ab
LucaMilla <61246777+LucaMilla@users.noreply.github.com>
2022-09-23 15:12:40 -0300

    Erro loop infinito linha 83

--- Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
@@ -80,7 +80,7 @@ public class Troco {
 
         @Override
         public boolean hasNext() {
-            for (int i = 6; i >= 0; i++) {
+            for (int i = 6; i >= 0; i--) {
                 if (troco.papeisMoeda[i] != null) {
                     return true;
                 }
